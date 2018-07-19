6e84b09e49b4472af49708995a07f0f2443905ad
karolscut <34779238+karolscut@users.noreply.github.com>
2018-07-20 07:31:13 +0800

    delete

--- src/main/java/fizz/Constants.java
+++
@@ -1,7 +0,0 @@
-package fizz;
-
-public class Constants {
-	public static final String FIZZ = "Fizz";
-	public static final String BUZZ = "Buzz";
-	public static final String FIZZ_BUZZ = "FizzBuzz";
-}
